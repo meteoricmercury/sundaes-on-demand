5900e88792a4aa09780741dabf9cef8d904565b7
vader9 <meteoricmercury@gmail.com>
2022-11-13 18:44:55 +0530

    replacing get with find

--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Options from '../Options';
 
-test('displays image for each scoop from the server', () => {
+test('displays image for each scoop from the server', async () => {
   render(<Options optionType="scoops" />);
 
-  const scoopImages = screen.findAllByRole('img', { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   const altText = scoopImages.map((element) => element.alt);
